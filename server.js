73be52472cb76ac02af473a83bf3ef34b7436798
Georgia Milteer <gmilteer@gmail.com>
2020-10-18 21:40:17 -0400

    saved changes

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb+srv://gmilteer:Myrtleamy12@cluster0.gpnge.mongodb.net/Budget-Tracker?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((result) => {
+    app.listen(PORT);
+    console.log("http://localhost:" + PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// routes
+app.use(require("./routes/api.js"));
