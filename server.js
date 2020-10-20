2159dfe381ee9ce31bfaf39e2edcdd65b6d9a10d
Georgia Milteer <gmilteer@gmail.com>
2020-10-19 21:15:22 -0400

    saved changes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static("public"));
 // routes
 app.use(require("./routes/api.js"));
 
